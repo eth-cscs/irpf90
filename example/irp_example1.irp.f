44a27ce0791580955c1b18cd04e7645eb2cb886a
Anthony Scemama <scemama@users.sourceforge.net>
2012-06-04 17:10:23 +0200

    Makefile with/without cython Version:1.2.13

--- example/irp_example1.irp.f
+++ example/irp_example1.irp.f
@@ -1,10 +1,12 @@
 program irp_example1
-  integer :: x(W)
-  BEGIN_SHELL [ /bin/bash ]
-    echo print *, \'Compiled by `whoami` on `date`\'
-    echo print *, \'$FC $FCFLAGS\'
-    echo print *, \'$IRPF90\'
-  END_SHELL
-! print *, 't = ', t
-  print *,  u2
+! integer :: x(W)
+! BEGIN_SHELL [ /bin/bash ]
+!   echo print *, \'Compiled by `whoami` on `date`\'
+!   echo print *, \'$FC $FCFLAGS\'
+!   echo print *, \'$IRPF90\'
+! END_SHELL
+  print *, 't = ', t
+
+  print *,  'v=', v
+! print *,  u2
 end
