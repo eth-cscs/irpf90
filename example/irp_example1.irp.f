8479ee0e38ffa3827c6401105667fb0fd751e18f
Anthony Scemama <scemama@users.sourceforge.net>
2012-07-31 14:24:52 +0200

    Broken link Version:1.2.17
    Version:1.2.18

--- example/irp_example1.irp.f
+++ example/irp_example1.irp.f
@@ -1,10 +1,10 @@
 program irp_example1
 ! integer :: x(W)
-! BEGIN_SHELL [ /bin/bash ]
-!   echo print *, \'Compiled by `whoami` on `date`\'
-!   echo print *, \'$FC $FCFLAGS\'
-!   echo print *, \'$IRPF90\'
-! END_SHELL
+  BEGIN_SHELL [ /bin/bash ]
+    echo print *, \'Compiled by `whoami` on `date`\'
+    echo print *, \'$FC $FCFLAGS\'
+    echo print *, \'$IRPF90\'
+  END_SHELL
   print *, 't = ', t
 
   print *,  'v=', v
